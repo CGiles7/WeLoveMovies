7aee971770d671980766f947476e15f8120f8e79
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-29 16:32:17 -0400

    update movies controller, router

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,4 +1,34 @@
-const path = require("path");
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-//use the existing movies data
-const movies = require(path.resolve(""))
+async function movieExists(req, res, next) {
+    const {movieId} = req.params;
+    const adta = await service.read(movieId);
+    if (data) {
+        res.locals.movie = data;
+        return next();
+    }
+    return next({status:404, message:'Cannot find movie.'})
+}
+
+async function list(req, res, next) {
+    const isShowing = req.query.is_showing;
+    if(isShowing) {
+        const data = await service.listMoviesShowing();
+        res.json({ data });
+    } else {
+        const data = await service.list();
+        res.json({ data });
+    }
+}
+
+async function read(req, res, next) {
+    const {movieId} = req.params;
+    const data = await service.read(movieId);
+    res.json({ data });
+}
+
+module.exports = {
+    list:[asyncErrorBoundary(list)],
+    read:[asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
+}
