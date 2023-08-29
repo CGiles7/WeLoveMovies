f67fe61d1b005c83c90bf8583656490adb95f125
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-29 11:39:41 -0400

    add movies.service, asyncErrorBoundary files

---
+++ src/movies/movies.service.js
@@ -0,0 +1,27 @@
+const knex = require("../db/connection");
+
+function list() {
+    return knex("movies").select("*");
+}
+
+function listMoviesShowing() {
+    return knex("movies as m")
+    .select("m.*")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .where("mt.is_showing", true)
+    .groupBy("m.movie_id")
+    .orderBy("m.movie_id");
+}
+
+function read(movieId) {
+    return knex("movies as m")
+    .select("*")
+    .where("m.movie_id", Number(movieId))
+    .first();
+}
+
+module.exports = {
+    list, 
+    listMoviesShowing,
+    read,
+};
