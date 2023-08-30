be8bdc572e81148a68c70d64edba6ea929b22bea
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-30 17:19:38 -0400

    update app, add reviews

---
+++ src/reviews/reviews.controller.js
@@ -0,0 +1,64 @@
+const service = require("./reviews.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const reduceProperties = require("../utils/reduce-properties");
+
+const reducedTheaterMovies1 = {
+    critic_id: ["critic", "critic_id"],
+    preferred_name: ["critic", "preferred_name"],
+    surname: ["critic", "surname"],
+    organization_name: ["critic", "organization_name"],
+    update_at: ["critic", "updated_at"],
+};
+
+const reducedTheaterMovies2 = {
+    preferred_name: ["critic", "preferred_name"],
+    surname: ["critic", "surname"],
+    organization_name: ["critic", "organization_name"],
+};
+
+async function reviewExists(req, res, next) {
+    const {reviewId} = req.parms;
+    const data = await service.readReview(reviewId);
+    if(data) {
+        res.locals.review = data;
+        return next();
+    }
+    return next({status:404, message:'Cannot find review'});
+}
+
+async function list(req, res, next) {
+    const {movieId} = req.parms;
+    if(movieId) {
+        let data = await service.read(movieId);
+        const reducer = reduceProperties("review_id", reducedTheaterMovies1);
+        data = reducer(data);
+        res.json({ data });
+    } else  {
+        const data = await service.list();
+        res.json({ data });
+    }
+}
+
+async function update(req, res, next) {
+    const updatedReview = {
+        ...res.locals.review,
+        ...req.body.data,
+        review_id: res.locals.review.review_id,
+    };
+    let data = await service.update(updatedReview);
+    const reduce = reduceProperties("review_id", reducedTheaterMovies2);
+    data = reducer(data);
+    res.json({ data });
+}
+
+async function destroy(req, res, next) {
+    const {review_id} = res.locals.review;
+    const data = await service.delete(review_id);
+    res.status(204).json({});
+}
+
+module.exports = {
+    list: asyncErrorBoundary(list),
+    update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
+    delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+}
---
+++ src/reviews/reviews.service.js
@@ -0,0 +1,43 @@
+const knex = require("../db/connection");
+
+function list() {
+    return knex("reviews").select("*");
+}
+
+function read(movieId) {
+    return knex("reviews as r")
+        .select("*")
+        .join("critics as c", "r.critic_id", "=", "c.critic_id")
+        .where("r.movie_id", Number(movieId));
+}
+
+function readReview(reviewId) {
+    return knex("reviews as r")
+        .select("*")
+        .where("r.review_id", Number(reviewId))
+        .first();
+}
+
+function update(updatedReview) {
+    return knex("reviews as r")
+        .where({review_id: updatedReview.review_id})
+        .update(updatedReview)
+        .then(() => {
+            return knex("reviews as r")
+                .join("critics as c", "r.critic_id", "c.critic_id")
+                .select(["r.*", "c.*"])
+                .where("r.review_id", updatedReview.review_id);
+        });
+}
+
+function destroy(review_id) {
+    return knex("reviews").where({review_id}).del();
+}
+
+module.exports = {
+    list, 
+    read,
+    readReview,
+    update,
+    delete:destroy,
+}
