b5f51b9895bd5aceb505683217fa8d1ba6671304
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-04 18:42:30 -0400

    update movies router

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
-router.route("/:movieId").get(controller.read);
-router.route("/:movieId/theaters").get(controller.listTheatersForMovie);
-router.route("/:movieId/reviews").get(controller.listReviewsForMovie);
+router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+router.route("/:movieId/theaters").get(controller.listTheatersForMovie).all(methodNotAllowed);
+router.route("/:movieId/reviews").get(controller.listReviewsForMovie).all(methodNotAllowed);
 
 module.exports = router;
