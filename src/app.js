9562e8c6c4ea2d15bf46b775594db337f72812ea
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-04 17:14:04 -0400

    update app file

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
-const theatersRouter = require("./theaters/theater.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
@@ -13,12 +13,11 @@ const errorHandler = require("./errors/errorHandler");
 app.use(cors());
 app.use(express.json());
 
-app.use("./movies", moviesRouter);
-app.use("./reviews", reviewsRouter);
-app.use("./theaters", theatersRouter);
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 module.exports = app;
