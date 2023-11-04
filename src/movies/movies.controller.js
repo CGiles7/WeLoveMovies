f44f8d9884c2289ded0671ce29fff71654d269ce
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-04 17:18:16 -0400

    update list function

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -13,18 +13,15 @@ async function movieExists(req, res, next) {
 
 async function list(req, res, next) {
     const isShowing = req.query.is_showing;
-    let data;
-  
-    if (isShowing === "true") {
-      // Handle showing movies
-      data = await service.listMoviesShowing();
+
+    if (isShowing === 'true') {
+        const data = await service.listMoviesShowing();
+        res.json({ data });
     } else {
-      // Handle all movies
-      data = await service.list();
+        const data = await service.list();
+        res.json({ data });
     }
-  
-    res.json({ data });
-  }  
+} 
 
 async function read(req, res, next) {
     const {movieId} = req.params;
