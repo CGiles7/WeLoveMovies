5e2822d3c9d7d21ead21e7426a86db24ffe81409
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-04 18:24:44 -0400

    update movies controller exports

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -3,18 +3,20 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(req, res, next) {
     const { movieId } = req.params;
-    const data = await service.read(movieId); // Fix the typo
+    const data = await service.read(movieId);
+
     if (data) {
         res.locals.movie = data;
         return next();
     }
+
     return next({ status: 404, message: 'Cannot find movie.' });
 }
 
 async function list(req, res, next) {
     const isShowing = req.query.is_showing;
 
-    if (isShowing === true) {
+    if (isShowing === true) { 
         const data = await service.listMoviesShowing();
         res.json({ data });
     } else {
@@ -24,12 +26,12 @@ async function list(req, res, next) {
 }
 
 async function read(req, res, next) {
-    const {movieId} = req.params;
+    const { movieId } = req.params;
     const data = await service.read(movieId);
     res.json({ data });
 }
 
 module.exports = {
-    list:[asyncErrorBoundary(list)],
-    read:[asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
-}
+    list: asyncErrorBoundary(list),
+    read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
+};
