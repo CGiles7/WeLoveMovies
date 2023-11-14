de6cc6ee2f540e535167f5a88b33daff6ecaa49f
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-14 12:05:17 -0500

    update error handlers in app

--- src/app.js
+++ src/app.js
@@ -5,22 +5,15 @@ const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 app.use(require("cors")());
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
 app.use("/reviews", reviewsRouter);
-
-// Not-found handler
-app.use((request, _response, next) => {
-  next({ status: 404, message: `Not found: ${request.originalUrl}` });
-});
-
-// Error handler
-app.use((error, _request, response, _next) => {
-  const { status = 500, message = "Something went wrong!" } = error;
-  response.status(status).json({ error: message });
-});
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
